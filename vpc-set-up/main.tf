5bf90b9d57277d6303d6f6a428d82d5f1d56a19f
Evan Woo <evanwoo327@gmail.com>
2024-08-23 17:26:58 +0200

    Two modules config

--- vpc-set-up/main.tf
+++ vpc-set-up/main.tf
@@ -21,137 +21,52 @@ terraform {
   }
 }
 
-data "aws_vpc" "default_vpc" {
-  default = true
+variable "db_pass_1" {
+  description = "password for database #1"
+  type        = string
+  sensitive   = true
 }
 
-
-
-data "aws_subnet_ids" "default_subnet" {
-  vpc_id = data.aws_vpc.default_vpc.id
+variable "db_pass_2" {
+  description = "password for database #2"
+  type        = string
+  sensitive   = true
 }
 
-resource "aws_security_group" "instances" {
-  name = "instance-security-group"
+variable "app-name-1" {
+    description = "Name of the web app"
+    type = string
 }
 
-resource "aws_security_group_rule" "allow_http_inbound" {
-  type              = "ingress"
-  security_group_id = aws_security_group.instances.id
-
-  from_port   = 8080
-  to_port     = 8080
-  protocol    = "tcp"
-  cidr_blocks = ["0.0.0.0/0"]
+variable "app-name-2" {
+    description = "Name of the web app"
+    type = string
 }
 
-resource "aws_lb_listener" "http" {
-  load_balancer_arn = aws_lb.load_balancer.arn
-  port = 80
-  protocol = "HTTP"
-
-  default_action {
-    type = "fixed-response"
-    fixed_response {
-      content_type = "text/plain"
-      message_body = "404: page not found"
-      status_code = 404
-    }
-  }
+module "web_app_1" {
+  source = "../vpc-set-up-modules"
+  # Input Variables
+  #bucket = "terraform-week-inyongwoo327"
+  domain = "evanwoo.com"
+  app-name         = var.app-name-1
+  environment_name = "production"
+  instance_type    = "t2.micro"
+  create_dns_zone  = true
+  db_name          = "webapp1db"
+  db_user          = "foo"
+  db_pass          = var.db_pass_1
 }
 
-resource "aws_lb_target_group" "instances" {
-  name     = "example-target-group"
-  port     = 8080
-  protocol = "HTTP"
-  vpc_id   = data.aws_vpc.default_vpc.id
-
-  health_check {
-    path                = "/"
-    protocol            = "HTTP"
-    matcher             = "200"
-    interval            = 15
-    timeout             = 3
-    healthy_threshold   = 2
-
-  } 
+module "web_app_2" {
+  source = "../vpc-set-up-modules"
+  # Input Variables
+  #bucket_prefix    = "web-app-2-data"
+  domain           = "evanwoo.com"
+  app-name         = var.app-name-2
+  environment_name = "production"
+  instance_type    = "t2.micro"
+  create_dns_zone  = true
+  db_name          = "webapp2db"
+  db_user          = "bar"
+  db_pass          = var.db_pass_2
 }
-
-resource "aws_lb_target_group_attachment" "instance_1" {
-  target_group_arn = aws_lb_target_group.instances.arn
-  target_id = aws_instance.instance_1.id
-  port = 8080
-}
-
-resource "aws_lb_target_group_attachment" "instance_2" {
-  target_group_arn = aws_lb_target_group.instances.arn
-  target_id        = aws_instance.instance_2.id
-  port             = 8080
-}
-
-resource "aws_lb_listener_rule" "instances" {
-  listener_arn = aws_lb_listener.http.arn
-  priority     = 100
-
-  condition {
-    path_pattern {
-      values = ["*"]
-    }
-  }
-
-  action {
-    type             = "forward"
-    target_group_arn = aws_lb_target_group.instances.arn
-  }
-}
-
-resource "aws_security_group" "alb" {
-  name = "alb-security-group"
-}
-
-resource "aws_security_group_rule" "allow_alb_http_inbound" {
-  type              = "ingress"
-  security_group_id = aws_security_group.alb.id
-
-  from_port   = 80
-  to_port     = 80
-  protocol    = "tcp"
-  cidr_blocks = ["0.0.0.0/0"]
-
-}
-
-resource "aws_security_group_rule" "allow_alb_all_outbound" {
-  type              = "egress"
-  security_group_id = aws_security_group.alb.id
-
-  from_port   = 0
-  to_port     = 0
-  protocol    = "-1"
-  cidr_blocks = ["0.0.0.0/0"]
-
-}
-
-resource "aws_lb" "load_balancer" {
-  name               = "web-app-lb"
-  load_balancer_type = "application"
-  subnets            = data.aws_subnet_ids.default_subnet.ids
-  security_groups    = [aws_security_group.alb.id]
-
-}
-
-resource "aws_route53_zone" "primary" {
-  name = "evanwoo.com"
-}
-
-resource "aws_route53_record" "root" {
-  zone_id = aws_route53_zone.primary.zone_id
-  name = "evanwoo.com"
-  type = "A"
-
-  alias {
-    name = aws_lb.load_balancer.dns_name
-    zone_id = aws_lb.load_balancer.zone_id
-    evaluate_target_health = true
-  }
-}
-
